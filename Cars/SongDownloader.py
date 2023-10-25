151fc2d1578bc7ab5921dfac63555312e1a10c17
blasian01 <bronsonwoods0@gmail.com>
2023-10-25 02:46:32 -0500

    Completed GPT response and placement into the deforum settings file
    issue with getting video export location

---
+++ Cars/SongDownloader.py
@@ -0,0 +1,55 @@
+from pytube import YouTube, Playlist
+from concurrent.futures import ThreadPoolExecutor
+import os
+import re
+import ssl
+# This program will be used to scrape youtube plalists to grab songs that you want to use in the video generation
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+def progress_callback(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage_of_completion = bytes_downloaded / total_size * 100
+    print(f"{stream.title} is {percentage_of_completion}% downloaded.")
+
+
+def sanitize_filename(filename):
+    return re.sub(r'[\\/*?:"<>|]', '', filename)
+
+
+def download_audio(youtube_link, output_path):
+    try:
+        yt = YouTube(youtube_link)
+        yt.register_on_progress_callback(progress_callback)
+        title = sanitize_filename(yt.title)
+        output_filename = f"{title}.mp3"
+        output_filepath = os.path.join(output_path, output_filename)
+        
+        if os.path.exists(output_filepath):
+            print(f"File {output_filename} already exists, skipping download.")
+            return
+        
+        stream = yt.streams.filter(only_audio=True).first()
+        stream.download(output_path=output_path, filename=output_filename)
+        
+    except Exception as e:
+        print(f"Error while downloading {youtube_link}: {e}")
+
+
+def main():
+    playlist_url = 'https://www.youtube.com/playlist?list=PLHg022HMFzFDMNp9xBGy3sARnqxaPl3PG'
+    output_path = 'output_folder/Music'
+
+    playlist = Playlist(playlist_url)
+
+    tasks = [(video_url, output_path) for video_url in playlist.video_urls]
+
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        for youtube_link, output_path in tasks:
+            executor.submit(download_audio, youtube_link, output_path)
+
+
+if __name__ == "__main__":
+    main()
--- Cars/mainCar.py
+++ Cars/mainCar.py
@@ -10,189 +10,30 @@ from moviepy.editor import *
 from api import *
 import moviepy.editor as mp
 import logging
+import subprocess
 from datetime import datetime
 
-
-openai.api_key = "OPEN_API_KEY"
-
-def generate_car_prompt(car_name):
-    prompt = f"""Generate a 900-second deform prompt for a cool car edit featuring a {car_name}. Make the format look like this but have the prompt in each text different.{{
-        "0": "{car_name} in a parking lot, gleaming in daylight, 'Ordinary Day' text overlay",
-        "50": "Interior of {car_name}, music blaring, GPS set, 'Ready to Roll' text",
-        "100": "Passenger in {car_name} messing with a GoPro, 'Capturing Moments' text",
-        "150": "GoPro leaning out the window of {car_name}, capturing the car's speed, 'Living on the Edge' text",
-        "200": "Rearview mirror view in {car_name} of multiple sports cars following, 'Drawing Attention' text",
-        "250": "Close-up of {car_name} driver's worried face, 'Something's Off' text",
-        "300": "Driver in {car_name} pulling over, sports cars surrounding, 'Showdown' text",
-        "350": "Driver in {car_name} struggling with seatbelt, tension rising, 'Critical Moments' text",
-        "400": "Bystander noticing {car_name}, 'Unexpected Savior' text",
-        "450": "Bystander in {car_name} talking to sports car drivers, 'Clearing the Air' text",
-        "500": "Drivers in {car_name} looking at GoPro footage, 'The Revelation' text",
-        "550": "Sports car drivers around {car_name} laughing, giving thumbs up, 'Misunderstanding Cleared' text",
-        "600": "{car_name} driver sitting back in car, sigh of relief, 'Close Call' text",
-        "650": "Driver in {car_name} checking glove box, realizing sunglasses are missing, 'The Real Loss' text",
-        "700": "{car_name} parked in a garage, driver contemplating, 'Reflecting' text",
-        "750": "GoPro in {car_name} placed on a shelf, 'Lesson Learned' text",
-        "800": "Driver polishing {car_name}, 'New Beginnings' text",
-        "850": "{car_name} text about responsible driving and filming, 'Drive Safe, Capture Wisely' text",
-        "900": "Montage of {car_name}'s journey, 'An Ordinary Drive Made Extraordinary' text overlay, epic music crescendo"
-    }}"""
-
-    try:
-        completion = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=prompt,
-            max_tokens=600
-        )
-        text = completion.choices[0].text.strip()
-        print(text)
-        return completion.choices[0].text.strip()
-    except Exception as e:
-        import traceback
-        print(traceback.format_exc())
-        return None
-
-
-
-# List of different cars
-cars = [
-    "Porsche gt3",
-    "Ferrari 488",
-    "Lamborghini Huracan",
-    "Tesla Model S",
-    "McLaren P1",
-    "Bugatti Chiron",
-    "Aston Martin Vantage",
-    "Chevrolet Corvette Z06",
-    "Dodge Viper",
-    "Nissan GT-R",
-    "BMW M4",
-    "Ford Mustang Shelby GT500",
-    "Audi R8",
-    "Jaguar F-Type",
-    "Mercedes-AMG GT",
-    "Alfa Romeo 4C",
-    "Koenigsegg Jesko",
-    "Pagani Huayra",
-    "Lotus Evora",
-    "Porsche 918 Spyder",
-    "Lamborghini Aventador",
-    "Ferrari LaFerrari",
-    "McLaren 720S",
-    "Bugatti Veyron",
-    "Aston Martin DBS",
-    "Chevrolet Camaro ZL1",
-    "Maserati GranTurismo",
-    "BMW M8",
-    "Dodge Challenger Hellcat",
-    "Ford GT",
-    "Audi RS 6 Avant",
-    "Cadillac CTS-V",
-    "Lexus LC 500",
-    "Mercedes-Benz SLR McLaren",
-    "Alpine A110",
-    "Koenigsegg Regera",
-    "Pagani Zonda",
-    "Lotus Exige",
-    "Porsche Carrera GT",
-    "Lamborghini Gallardo",
-    "Ferrari F40",
-    "McLaren F1",
-    "TVR Sagaris",
-    "Aston Martin One-77",
-    "Chevrolet Corvette Stingray",
-    "Maserati MC12",
-    "BMW M5",
-    "Dodge Charger SRT Hellcat",
-    "Audi RS 7",
-    "Jaguar XKR",
-    "Mercedes-Benz SLS AMG",
-    "Noble M600",
-    "Koenigsegg Agera",
-    "Pagani Huayra Roadster",
-    "Lotus Elise",
-    "Porsche Taycan",
-    "Lamborghini Murcielago",
-    "Ferrari F50",
-    "McLaren 600LT",
-    "Rimac C_Two",
-    "Aston Martin DB11",
-    "Chevrolet Corvette C8",
-    "Maserati Alfieri",
-    "BMW i8",
-    "Dodge Challenger SRT Demon",
-    "Audi TT RS",
-    "Jaguar XJ220",
-    "Mercedes-Benz CLK GTR",
-    "W Motors Lykan Hypersport",
-    "Koenigsegg Gemera",
-    "Pagani Huayra BC",
-    "Lotus Esprit",
-    "Porsche Boxster",
-    "Lamborghini Diablo",
-    "Ferrari Enzo",
-    "McLaren 570S",
-    "Rimac Nevera",
-    "Aston Martin Valkyrie",
-    "Chevrolet Camaro SS",
-    "Maserati Quattroporte",
-    "BMW Z4",
-    "Dodge Viper ACR",
-    "Audi S5",
-    "Jaguar XK",
-    "Mercedes-AMG Project One",
-    "W Motors Fenyr SuperSport",
-    "Koenigsegg One:1",
-    "SSC Tuatara",
-    "Lotus 3-Eleven",
-    "Porsche Panamera",
-    "Lamborghini Veneno",
-    "Ferrari 812 Superfast",
-    "McLaren GT",
-    "Rimac Concept One",
-    "Aston Martin Rapide",
-    "Chevrolet Camaro IROC-Z",
-    "Maserati Levante Trofeo",
-    "BMW M2",
-    "Dodge Dart Swinger",
-    "Audi RS Q8",
-]
-
-# # temp prompt for testing
-# generated_prompt = {
-# "0": "Robotic explorer booting up on an alien terrain",
-# "50": "Explorer navigating through rocky outcrops",
-# "100": "Explorer analyzing exotic flora with built-in sensors",
-# "150": "Explorer trekking towards a distant water source",
-# "200": "Explorer observing alien fauna near the water",
-# "250": "Explorer collecting water samples for analysis",
-# "300": "Explorer encountering a sandstorm, initiating protective shields",
-# "350": "Explorer traversing through a field of geysers",
-# "400": "Explorer scaling a steep hill to gain a vantage point",
-# "450": "Explorer capturing panoramic views of the alien landscape",
-# "500": "Explorer discovering ancient alien ruins",
-# "550": "Explorer decoding alien inscriptions on the ruins",
-# "600": "Explorer uncovering a hidden underground cave",
-# "650": "Explorer mapping the cave's extensive tunnel network",
-# "700": "Explorer finding rare minerals in the cave",
-# "750": "Explorer transmitting data back to Earth",
-# "800": "Explorer powering down, mission accomplished"
-# }
 #----------------------------------------------------------------------------------------------------
 
 # Replacing prompt with new deform prompt from GPT
-def replace_prompts(file_path, new_prompts):
-    # Load the existing data
-    with open(file_path, 'r') as file:
-        data = json.load(file)
-
-    # Replace the 'prompts' field with new values
-    data['prompts'] = new_prompts
-
-    # Save the modified data back to the file
-    with open(file_path, 'w') as file:
-        json.dump(data, file, indent=4)  # indent for pretty formatting
+def replace_prompts(target_file, source_file):
+    # Load the content from the source file
+    with open(source_file, 'r', encoding='utf-8') as file:
+        source_content = json.load(file)
+    
+    # Load the content from the target file
+    with open(target_file, 'r', encoding='utf-8') as file:
+        target_content = json.load(file)
+    
+    # Replace the 'prompts' section in the target content with the source content
+    target_content['prompts'] = source_content
+    
+    # Write the updated content back to the target file
+    with open(target_file, 'w', encoding='utf-8') as file:
+        json.dump(target_content, file, indent=4)
         
+#-----------------------------------------------------------------------------------------------
+
 # Functions for creating video with sound
 def get_latest_file(folder_path):
     files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
@@ -230,26 +71,28 @@ def add_audio_to_video(video_path, audio_path, output_path):
         print(f"Successfully combined {video_path} with {audio_path}. Output saved to {output_path}.")
     except Exception as e:
         print(f"Error writing video file: {e}")
-        
+
+     
 #----------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    
-    # picking random car
-    random_car = random.choice(cars)
 
-    #Making Diffusion Prompt for car
-    print(f"Generating prompt for {random_car}...")
-    generated_prompt = generate_car_prompt(random_car)
+    #Making Diffusion Prompt for the car that was selected
+    #subprocess.run(["python", "prompt.py"])
+    with open("output_folder/prompt.txt") as f:
+        generated_prompt = f.read()
     print(generated_prompt)
+    generated_prompt = generated_prompt + ","
+
     
     # Setting the path for deforum settings
     settings_file_path = 'deforum_settings.txt'
+    promptFile = 'output_folder/prompt.txt'
 
     # Call the function to replace the prompts
-    replace_prompts(settings_file_path, generated_prompt)
+    replace_prompts(settings_file_path, promptFile) # Failing to properly input the new prompt
     
-    job_id = send_post_request('deforum_settings.txt', 'output_folder')
+    job_id = send_post_request('deforum_settings.txt', 'output_folder') 
     if job_id:
         print(f"The job_id is: {job_id}")
         output_directory = get_output_directory(job_id)
@@ -260,7 +103,7 @@ if __name__ == "__main__":
         print("Failed to get the job_id.")
     
     # File locations for video and audio
-    video_folder = "output_folder"
+    video_folder = "output_folder/RawVideo"
     audio_folder = "output_folder/Music"
     
     # Get the current date and format it as YYYYMMDD
